5229d0411ba125066afaa392139cc1717ab40882
langzi <1154505909@qq.com>
2016-06-15 11:36:22 +0800

    scrapy study

---
+++ scrapytest/scrapytest/spiders/dmoz_spider.py
@@ -0,0 +1,9 @@
+from scrapy.spider import Spider
+
+class DmozSpider(Spider):
+        name="dmoz"
+        allowed_domains=['dmoz.org']
+        start_url=["http://www.dmoz.org/Computers/Programming/Languages/Python/Books/","http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"]
+        def parse(self,response):
+                filename=response.url.split("/")[-2]
+                open(filename,'wb').write(response.body)
