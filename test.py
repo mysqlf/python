8c1e4dd351205fa9e59d7fa990e9f1f0603a0c9b
langzi <1154505909@qq.com>
2016-08-22 09:52:49 +0800

    单元测试

--- test.py
+++ test.py
@@ -120,4 +120,47 @@ import unittest
 # # print(d['a'])
 # if __name__=='__main__':#运行单元测试的方法
 #     unittest.main()
-    
+#     
+#文档测试  
+import re
+#Python内置的“文档测试”（doctest）模块可以直接提取注释中的代码并执行测试。
+# m=re.search('(?<=abc)def','abcdef')
+# print(m.group(0))
+class Dict(dict):
+    '''
+    Simple dict but also support access as x.y style.
+
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    '''
+    def __init__(self, **kw):
+        super(Dict, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+if __name__=='__main__':
+    import doctest#只有在引入了dotest才会运行文档中的测试代码,其他时间不会执行
+    doctest.testmod()
