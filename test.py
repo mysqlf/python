d53d4677378d736015bb0f598f539b3a332eeb14
langzi <1154505909@qq.com>
2016-09-23 09:06:09 +0800

    PEP*

--- test.py
+++ test.py
@@ -7,7 +7,7 @@
 # if __name__=='__main__':
 #     my_func()
 
-#异常处理
+# 异常处理
 # try:
 #     print('try...')
 #     r=10/0
@@ -46,14 +46,14 @@
 # print(r)
 # test(0)
 # test('q')
-#调试方式
-import logging#logging方式 主要方法还是使用log
+# 调试方式
+import logging  # logging方式 主要方法还是使用log
 logging.basicConfig(level=logging.INFO)
 # s='0'
 # n=int(s)
 # logging.info('n=%d'%n)
 # print(10/n)
-# 
+#
 # def foo(s):#assert 方式
 #     n=int(s)
 #     assert n!=0, 'n is zero'
@@ -61,7 +61,7 @@ logging.basicConfig(level=logging.INFO)
 # def main():
 #     foo(0)
 # main()
-# 
+#
 # s='0'
 # n=int(s)
 # print(10/n)
@@ -71,9 +71,9 @@ logging.basicConfig(level=logging.INFO)
 # pdb.set_trace()
 # print(10/n)
 
-#单元测试
+# 单元测试
 #
-#被测试的类
+# 被测试的类
 # class Dict(dict):
 #     def __init__(self,**kw):
 #         super().__init__(**kw)
@@ -87,46 +87,57 @@ logging.basicConfig(level=logging.INFO)
 
 import unittest
 
-# class TestDict(unittest.TestCase):
-#     def setUp(self):#可以在单元测试中编写两个特殊的setUp()和tearDown()方法。这两个方法会分别在每调用一个测试方法的前后分别被执行。
-#         print('setUp')
-#     def tearDown(self):
-#         print('tetsDown...')
-#     def test_unit(self):
-#         d=Dict(a=1,b='test')
-#         self.assertEqual(d.a,1)#断言输出是否是我们所期望的,覆盖所有的情况
-#         self.assertEqual(d.b,'test')
-#         self.assertTrue(isinstance(d,dict))
-#     def test_key(self):
-#         d=Dict()
-#         d['key']='value'
-#         self.assertEqual(d.key,'value')
-#     def test_attr(self):
-#         d=Dict()
-#         d.key='value'
-#         self.assertTrue('key' in d)
-#         self.assertEqual(d['key'],'value')
-#     def test_keyerror(self):
-#         d=Dict()
-#         with self.assertRaises(KeyError):#另一种重要的断言就是期待抛出指定类型的Error，比如通过d['empty']访问不存在的key时，断言会抛出KeyError：
-#             value=d['empty']
-#     def test_attrerror(self):
-#         d=Dict()
-#         with self.assertRaises(AttributeError):
-#             value=d.empty
+
+class TestDict(unittest.TestCase):
+
+    def setUp(self):  # 可以在单元测试中编写两个特殊的setUp()和 tearDown() 方法。这两个方法会分别在每调用一个测试方法的前后分别被执行。
+        print('setUp')
+
+    def tearDown(self):
+        print('tetsDown...')
+
+    def test_unit(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)  # 断言输出是否是我们所期望的,覆盖所有的情况
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key, 'value')
+
+    def test_attr(self):
+        d = Dict()
+        d.key = 'value'
+        self.assertTrue('key' in d)
+        self.assertEqual(d['key'], 'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):  # 另一种重要的断言就是期待抛出指定类型的Error，比如通过d['empty']访问不存在的key时，断言会抛出KeyError：
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
 
 
 # #d=Dict(a=1,b=2)
 # # print(d['a'])
 # if __name__=='__main__':#运行单元测试的方法
 #     unittest.main()
-#     
-#文档测试  
+#
+# 文档测试
 import re
-#Python内置的“文档测试”（doctest）模块可以直接提取注释中的代码并执行测试。
+# Python内置的“文档测试”（doctest）模块可以直接提取注释中的代码并执行测试。
 # m=re.search('(?<=abc)def','abcdef')
 # print(m.group(0))
+
+
 class Dict(dict):
+
     '''
     Simple dict but also support access as x.y style.
 
@@ -149,6 +160,7 @@ class Dict(dict):
         ...
     AttributeError: 'Dict' object has no attribute 'empty'
     '''
+
     def __init__(self, **kw):
         super(Dict, self).__init__(**kw)
 
@@ -161,6 +173,6 @@ class Dict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-if __name__=='__main__':
-    import doctest#只有在引入了dotest才会运行文档中的测试代码,其他时间不会执行
+if __name__ == '__main__':
+    import doctest  # 只有在引入了dotest才会运行文档中的测试代码,其他时间不会执行
     doctest.testmod()
