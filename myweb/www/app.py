508f849a685393afdcd91239e7bf2452a6449db9
langzi <1154505909@qq.com>
2016-10-11 10:47:12 +0800

    orm

--- myweb/www/app.py
+++ myweb/www/app.py
@@ -6,7 +6,7 @@ import asyncio
 import os
 import json
 import time
-import aiomysql
+
 from datetime import datetime
 from aiohttp import web
 
@@ -25,54 +25,3 @@ def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
-
-
-@asyncio.coroutine
-def create_pool(loop, **kw):
-    logging.info('create datebase connection pool')
-    global __pool
-    __pool = yield from aiomysql.create_pool(
-        host=kw.get('host', 'localhost'),
-        port=kw.get('port', 3306),
-        user=kw['user'],
-        password=kw['password'],
-        db=kw['db'],
-        charset=kw.get('charset', 'utf8'),
-        autocommit=kw.get('autocommit', True),
-        maxsize=kw.get('maxsize', 10),
-        minsize=kw.get('minsize', 1),
-        loop=loop
-    )
-
-# sql语句的占位符是'?'
-# mysql的占位符是'%s'
-
-
-@asyncio.coroutine
-def select(sql, args, size=None):
-    log(sql, args)
-    global __pool
-    with (yield from __pool) as conn:
-        cur = yield from conn.cursor(aiomysql.DictCursor)
-        yield from cur.execute(sql.replace('?', '%s'), args or ())
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
-        logging.info('row returned:%s' % len(rs))
-        return rs
-
-
-@asyncio.coroutine
-def execute(sql, args):
-    log(sql)
-    with (yield from __pool) as conn:
-        try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?', '%s'), args)
-            affected = cur.rowcount
-            yield from cur.close()
-        except BaseException as e:
-            raise
-        return affected
