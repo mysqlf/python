6f15607e7a2945fa66a403bfe712d663f88bec47
Greedy_wolf <1154505909@qq.com>
2016-10-20 08:00:21 +0800

    add login test

--- flaskuse/flask_test.py
+++ flaskuse/flask_test.py
@@ -21,5 +21,21 @@ class FlaskrTestCase(unittest.TestCase):
     def test_empty_db(self):
         rv = self.app.get('/')
         assert 'No enteries here so far 'in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(username=username, password=password), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'Your were logged in ' in rv.data
+        rv = self.logout()
+        assert 'Your were loggrd out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert 'Username error' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Password error' in rv.data
 if __name__ == '__main__':
     unittest.main()
