6e1980836854175168822969e1d48bfd48af34ac
langzi <1154505909@qq.com>
2017-03-24 18:32:07 +0800

    add string token

--- home/second/token.py
+++ home/second/token.py
@@ -3,8 +3,8 @@
 # 字符串令牌解析
 import re
 text = 'foo = 23 + 42 * 10'
-tokens = [('NAME', 'foo'), ('EQ', '='), ('NUM', '23'), ('PLUS', '+'),
-          ('NUM', '42'), ('TIMES', '*'), ('NUM', '10')]
+# tokens = [('NAME', 'foo'), ('EQ', '='), ('NUM', '23'), ('PLUS', '+'),
+#           ('NUM', '42'), ('TIMES', '*'), ('NUM', '10')]
 NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
 NUM = r'(?P<NUM>\d+)'
 PLUS = r'(?P<PLUS>\+)'
@@ -14,39 +14,40 @@ WS = r'(?P<WS>\s+)'
 master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
 
 
-scanner = master_pat.scanner('foo=42+23')
-tmp = scanner.match()
-print(tmp)
-print(tmp.lastgroup, tmp.group())
-tmp = scanner.match()
-print(tmp)
-print(tmp.lastgroup, tmp.group())
-tmp = scanner.match()
-print(tmp)
-print(tmp.lastgroup, tmp.group())
-tmp = scanner.match()
-print(tmp)
-print(tmp.lastgroup, tmp.group())
-tmp = scanner.match()
-print(tmp)
-print(tmp.lastgroup, tmp.group())
-# tmp = scanner.match()
-# print(tmp)
-# print(scanner.match().lastgroup, scanner.match().group())
-
 from collections import namedtuple
+
+Token = namedtuple('Token', ['type', 'value'])
 
 
 def generate_tokens(pat, text):
-    Token = namedtuple('Token', ['type', 'value'])
     scanner = pat.scanner(text)
     for m in iter(scanner.match, None):
-        yield Token(m.lastgroup, m.group())
+        tok = Token(m.lastgroup, m.group())
+        if tok.type != 'WS':
+            yield tok
 
-for tok in generate_tokens(master_pat, 'foo=42'):
-    print(tok)
 
-tokens = (tok for tok in generate_tokens(master_pat, text)
-          if tok.type != 'WS')
+tokens = (tok for tok in generate_tokens(master_pat, text))
+
 for tok in tokens:
     print(tok)
+
+
+# 这段是自己手动一步步解析token 如果超过长度继续解析会报错
+# 需要自己控制长度
+# scanner = master_pat.scanner('foo=42+23')
+# tmp = scanner.match()
+# print(tmp)
+# print(tmp.lastgroup, tmp.group())
+# tmp = scanner.match()
+# print(tmp)
+# print(tmp.lastgroup, tmp.group())
+# tmp = scanner.match()
+# print(tmp)
+# print(tmp.lastgroup, tmp.group())
+# tmp = scanner.match()
+# print(tmp)
+# print(tmp.lastgroup, tmp.group())
+# tmp = scanner.match()
+# print(tmp)
+# print(tmp.lastgroup, tmp.group())
