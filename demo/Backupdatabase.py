964d9afe92131f994f3cb5be5980ec0552cbdb22
ZL <1154505909@qq.com>
2017-06-01 16:58:20 +0800

    完善数据库导入

--- demo/Backupdatabase.py
+++ demo/Backupdatabase.py
@@ -9,30 +9,36 @@ import os
 """
   Purpose: 备份数据库
 """
-dbUser='test'
-dbPasswd='test'
-#不能使用空密码的用户导出数据库
-dbHost='127.0.0.1'
-dbCharset = 'utf8'
-backupDir = 'E:\\'
-backupDate =  time.strftime("%Y%m%d")
+def backup_database(backupDate=''):
+    dbUser='test'
+    #不能使用空密码的用户导出数据库
+    dbPasswd='test'
+    dbHost='127.0.0.1'
+    dbCharset = 'utf8'
+    backupDir = 'E:\\'
+    if len(backupDate)==0:
+        backupDate =  time.strftime("%Y%m%d\\%H-%M")
+    print ('The database backup to start! %s'%time.strftime('%Y-%m-%d %H:%M:%S'))
+    # 打开数据库连接
+    db = pymysql.connect(dbHost,dbUser,dbPasswd,'')
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    #查出MySQL中所有的数据库名称
+    sqlStr1 = "show databases"
+    #执行查询
+    cursor.execute(sqlStr1)
+    allDatabase = cursor.fetchall()
+    #根据日期时间生成目录
+    path=backupDir+backupDate
+    if not os.path.exists(path):
+        os.makedirs(path)
+    #循环导出
+    for db in allDatabase:
+        dbName=db[0]
+        fileName = '%s\\%s.sql' %(path,dbName)
+        if os.path.exists(fileName):
+            os.remove(fileName)
+        os.system("mysqldump -h%s -u%s -p%s %s --default_character-set=%s > %s" %(dbHost,dbUser,dbPasswd,dbName,dbCharset,fileName))
+    print ('The database backup success! %s'%time.strftime('%Y-%m-%d %H:%M:%S'))
 
-print ('The database backup to start! %s'   %time.strftime('%Y-%m-%d %H:%M:%S'))
-# 打开数据库连接
-db = pymysql.connect(dbHost,dbUser,dbPasswd,'')
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
-#查出MySQL中所有的数据库名称
-sqlStr1 = "show databases"
-#执行查询
-cursor.execute(sqlStr1)
-allDatabase = cursor.fetchall()
-#循环导出
-for db in allDatabase:
-    dbName=db[0]
-    fileName = '%s\\%s_%s.sql' %(backupDir,backupDate,dbName)
-    if os.path.exists(fileName):
-        os.remove(fileName)
-    os.system("mysqldump -h%s -u%s -p%s %s --default_character-set=%s > %s/%s_%s.sql" %(dbHost,dbUser,dbPasswd,dbName,dbCharset,backupDir,backupDate,dbName))
-print ('The database backup success! %s'%time.strftime('%Y-%m-%d %H:%M:%S'))
-
+backup_database()
